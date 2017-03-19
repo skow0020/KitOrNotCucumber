4b35530c65d8b052d8e14286abce4d043cd4125c
skow0020 <skow0020@gmail.com>
2017-03-18 21:35:33 -0500

    Initial commit for adding to github - in progress

---
+++ src/main/java/Main.java
@@ -0,0 +1,65 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Colin on 3/12/2017.
+ */
+public class Main {
+    public static void main(String[] args) throws MalformedURLException {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //-----------------------------------------------Intro Tutorlial Stuff------------------------------------------
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        caps.setCapability("deviceName", "VS9854G9e1c8906");
+//        caps.setCapability("platformName", "Android");
+//        caps.setCapability("app", "C:\\Program Files (x86)\\Appium\\node_modules\\appium\\build\\unlock_apk\\unlock_apk-debug.apk");
+//        AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+//
+//        driver.pressKeyCode(AndroidKeyCode.HOME);
+////
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        driver.findElementByAndroidUIAutomator("new UiSelector().description(\"Apps\")").click();
+//        driver.findElementByAndroidUIAutomator("new UiSelector().description(\"Settings\")").click();
+//        driver.findElementByAndroidUIAutomator("new UiSelector().textContains(\"Wi\").textContains(\"Fi\")").click();
+//
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String ssid = driver.findElementByXPath("//android.widget.TextView[@text=\"Connected\"]/preceding::android.widget.TextView[1]").getAttribute("name");
+//
+//        System.out.println("Connected to: " + ssid);
+////
+////        driver.findElementByAndroidUIAutomator("new UiSelector().description(\"Habits\")").click();
+////        driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Bike to work\")").click();
+////        driver.findElementByAndroidUIAutomator("new UiSelector().description(\"Navigate up\")").click();
+//
+//        //driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"org.isoron.uhabits:id/action_edit_habit\").checked(true)").click();
+//        driver.quit();
+    }
+}
